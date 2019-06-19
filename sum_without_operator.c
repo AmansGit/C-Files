c87b4782558d65dd1659cc1624053251e167f84a
Aman Kumar Sharma <amans1997a@gmail.com>
2019-06-19 22:22:09 +0530

    Add files via upload

---
+++ sum_without_operator.c
@@ -0,0 +1,22 @@
+/*	author: Aman Kumar Sharma
+ *	Language used: C
+ *	Prob: Find the sum of any two number without using any operator..
+ _______________________________________________________________________
+ */
+
+
+#include<stdio.h> 
+  
+int add(int x, int y) 
+{ 
+    return printf("%*c%*c", x, ' ',y, ' ');	// * => field width specifier. 
+} 
+  
+// Driver code 
+int main() 
+{
+	int a,b;	
+	scanf("%d%d",&a,&b);
+	printf("\nSum = %d\n", add(a,b)); 
+	return 0; 
+}
